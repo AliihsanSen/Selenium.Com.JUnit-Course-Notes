21ef79143f0cf288530a72dcb8766d4b171f5514
Alı Ihsan SEN <av.aliihsansen@gmail.com>
2022-09-21 00:03:47 +0300

    21.09.2022

--- src/test/java/Day07_TestBase/C05_Actions.java
+++ src/test/java/Day07_TestBase/C05_Actions.java
@@ -1,6 +1,7 @@
 package Day07_TestBase;
 
 import Utilities.TestBaseBeforeAfter;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -8,7 +9,7 @@ import org.openqa.selenium.WebElement;
 public class C05_Actions extends TestBaseBeforeAfter {
 
     @Test
-    public void test() {
+    public void test() throws InterruptedException {
 
         // https://demoqa.com/droppable adresine gidelim
         driver.get("https://demoqa.com/droppable");
@@ -16,7 +17,12 @@ public class C05_Actions extends TestBaseBeforeAfter {
         // “Drag me” butonunu tutup “Drop here” kutusunun ustune
         WebElement dragMe = driver.findElement(By.xpath("//*[@id='draggable']"));
         WebElement dropHere = driver.findElement(By.xpath("(//*[@id='droppable'])[1]"));
+        Thread.sleep(2000);
+        actions.dragAndDrop(dragMe, dropHere).perform();
 
         // “Drop here” yazisi yerine “Dropped!” oldugunu test edin
+        WebElement text = driver.findElement(By.xpath("//*[text()='Dropped!']"));
+        String actualText = text.getText();
+        Assert.assertEquals("Dropped!",actualText);
     }
 }
